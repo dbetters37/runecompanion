14500aad9be250a5ef19393d5d83b3f0b02b13b7
dbetters37 <dbetters37@gmail.com>
2026-09-02 09:09:18 -0400

    Give Rune Companion Remix a unique package ID

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -14,7 +14,7 @@ android {
   compileSdk { version = release(36) { minorApiLevel = 1 } }
 
   defaultConfig {
-    applicationId = "com.aistudio.runepet.companion.vsbslr"
+    applicationId = "com.aistudio.runepet.companion.vsbslr.remix"
     minSdk = 24
     targetSdk = 36
     versionCode = 1
